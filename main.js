69bdcc76e68f7bdb1f6031fc302ec7030c68c46c
prince kashyap <vasukashyap08@gmail.com>
2023-06-16 17:34:00 +0530

    commit

--- main.js
+++ main.js
@@ -1,21 +1,7 @@
-$(".carousel").owlCarousel({
-    loop: true,
-    margin: 20,
-    autoplay: true,
-    autoplayTimeout: 2000,
-    autoplayHoverPause: true,
-    responsive: {
-        0: {
-            items: 3,
-            nav: false
-        },
-        600: {
-            items: 6,
-            nav: false
-        },
-        1000: {
-            items: 9,
-            nav: true
-        },
-    }
-});
+// Hamburger 
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+hamburger.addEventListener("click",()=>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
