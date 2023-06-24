636280c9d290f0e42f180339362bedbd3777a372
Prince kashyap <vasukashyap08@gmail.com>
2023-06-24 18:26:08 +0530

    commit

--- main.js
+++ main.js
@@ -4,4 +4,4 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click",()=>{
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-})
+});
